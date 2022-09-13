ad2f9676412bb3ce7aad22627f71573617d18c81
FirewallGR <qwert.ting@bk.ru>
2022-09-13 21:09:33 +0300

    commited homework

--- FirstClass/FirstClass/main.cpp
+++ FirstClass/FirstClass/main.cpp
@@ -1,46 +1,69 @@
 #include <iostream>
 #include <fstream>
-class Coordinates {
-	int x, y;
+
+template <class Type> class TCoords;
+
+template <class Type>
+class TCoords {
+	Type x;
+	Type y;
 public:
-	Coordinates(int x, int y) {
+	TCoords(Type x, Type y) : x(x), y(y) {
 		this->x = x;
 		this->y = y;
 	}
 	
-	Coordinates() {
+	TCoords() : x(0), y(0) {
 		x = y = 0;
 	}
 
-	Coordinates(const Coordinates &coord) {
+
+	template <class Type2>
+	TCoords(const TCoords<Type2>& coord) {
 		this->x = coord.x;
 		this->y = coord.y;
 	}
 
-	Coordinates& operator=(Coordinates coord) {
+	template <class Type2>
+	TCoords<Type>& operator=(const TCoords<Type2>& coord) {
 		if (this == &coord) return *this;
-		Coordinates temp(coord.x, coord.y);
-		return temp;
+		this->x = coord.x;
+		this->y = coord.y;
+		return *this;
 	}
-	Coordinates& operator+(Coordinates coord) {
-		Coordinates temp(this->x + coord.x, this->y + coord.y);
+
+	TCoords<Type>& operator+(TCoords<Type> coord) {
+		TCoords<Type> temp(this->x + coord.x, this->y + coord.y);
 		return temp;
 	}
 
-	Coordinates& operator-(Coordinates coord) {
-		Coordinates temp(this->x - coord.x, this->y - coord.y);
+	TCoords<Type>& operator-(TCoords<Type> coord) {
+		TCoords<Type> temp(this->x - coord.x, this->y - coord.y);
 		return temp;
 	}
-	std::ostream& operator<<(std::ostream& out) {
-		out << "X: " << this->x << "\nY: " << this->y << std::endl;
-		return out;
-	}
+
 
 	void print() {
 		std::cout << x << " " << y << std::endl;
 	}
+
+	friend std::ostream& operator<<(std::ostream& out, TCoords& coord) {
+		out << "X: " << coord.x << "\nY: " << coord.y << std::endl;
+		return out;
+	}
 };
 
+
 int main() {
+	TCoords<int> coords1(1, 0);
+	coords1.print();
+	std::cout << coords1;
+
+	TCoords<int> coords2(0, 1);
+
+	TCoords<int> coords3 = coords1 + coords2;
+
+	coords3.print();
+	std::cout << coords3;
 
 }
